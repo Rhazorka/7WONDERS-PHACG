8b893413c6a37d26dc674abfbfad7923ed031f45
HugoBrg <hugo.brg@outlook.com>
2019-02-12 08:51:21 +0100

    Proposition carte, choix de la carte, mélange des cartes, attribution des points

---
+++ 7WONDERS-PHACG/src/Moteur.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner; 
+
+public class Moteur {
+	private ArrayList<Carte> cartes = new ArrayList<>();
+
+	public Moteur(ArrayList<Carte> cartes) {
+		super();
+		this.cartes = cartes;
+	}
+	
+	public Carte choisirCarte() {
+		Carte ret;
+		System.out.println("Bonjour, choissisez une carte en appuyant sur le chiffre correspondant : ");
+		for (Carte c : cartes) {
+			 System.out.println(cartes.indexOf(c)+" : "+c.toString());
+		}
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			String str = sc.nextLine();
+			try {
+				int selection = Integer.parseInt(str);
+				ret = cartes.get(selection);
+				cartes.remove(selection);
+				return ret;
+			}catch(NumberFormatException e) {
+				System.out.println("Il faut que la selection soit un chiffre");
+			}catch(IndexOutOfBoundsException e) {
+				System.out.println("Il faut que le chiffre soit compris entre 0 et "+(cartes.size()-1));
+			}
+		}
+	}
+	
+	public void melangerCartes() {
+		Collections.shuffle(cartes);
+	}
+}
